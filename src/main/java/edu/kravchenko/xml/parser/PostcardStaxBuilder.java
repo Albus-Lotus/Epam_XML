26f531683cdffd36478a77dce66a8b322f72c35d
Polina Kravchenko <lotushiems@gmail.com>
2021-12-06 17:36:58 +0300

    add stax builder

---
+++ src/main/java/edu/kravchenko/xml/parser/PostcardStaxBuilder.java
@@ -0,0 +1,15 @@
+package edu.kravchenko.xml.parser;
+
+import edu.kravchenko.xml.exception.PostcardException;
+
+
+public class PostcardStaxBuilder extends PostcardBuilder {
+
+    public PostcardStaxBuilder() {
+    }
+
+    @Override
+    public void buildPostcards(String filePath) throws PostcardException {
+
+    }
+}
